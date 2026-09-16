9ae2c4dfa86759652720f6b8c01efb9b750c56eb
jjaass9507 <jjaass9507@gmail.com>
2026-09-16 10:12:51 +0800

    fix: place raft foundation at bottom and remove roof cap

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 import { processRawData } from './data.js';
 import { formatArea, apiUrl } from './utils.js';
 import { renderHeader, renderMatrix, renderPanel, renderCompareTable } from './components.js';
-import { renderBuilding3DModal, bindBuilding3DInteractions } from './building-3d.js?v=20260916-uniform-building-scale';
+import { renderBuilding3DModal, bindBuilding3DInteractions } from './building-3d.js?v=20260916-raft-bottom-no-roof-cap';
 
 // --- 狀態管理 (State) ---
 const state = {
